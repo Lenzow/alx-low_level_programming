5ac9e7406736795d2eeeb77faac2b1e129799c19
Lenzow <ayoubelgrine@gmail.com>
2023-07-13 17:24:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - my main function that do everything
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n")
+	return (0);
+}
