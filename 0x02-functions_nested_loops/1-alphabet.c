a9beb11918154e7662119985216e47081417e339
Lenzow <ayoubelgrine@gmail.com>
2023-07-18 15:47:34 -0700

    Alphabet a to z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - its printing the alphabets in lowercase ended by a new line!
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-
 void print_alphabet(void)
 {
-	char azletter;
-	for (azletter = 'a'; azletter <= 'z'; azletter++)
-		_putchar(azletter);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
 	_putchar('\n');
 }
